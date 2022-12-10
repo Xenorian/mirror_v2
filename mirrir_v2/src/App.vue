ae07cdaa29a92f711a49a2330680f9c286fe2d50
xenoria <1164903882@qq.com>
2022-12-10 18:53:37 +0800

    Initial commit

---
+++ mirrir_v2/src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import SearchBar from './components/SearchBar.vue'
+import Hello from './components/helloSection.vue'
+</script>
+
+<template>
+  <Hello class="banner" />
+  <SearchBar class="searchBar"/>
+</template>
+
+<style scoped>
+.banner {
+  max-height: 400px;
+}
+
+.searchBar{
+  max-width: 400px;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
---
+++ mirrir_v2/src/components/SearchBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="mt-4">
+        <el-input v-model="inputsearch" placeholder="Please input" class="input-with-select">
+            <template #append>
+                <el-button :icon="Search" @click="onsearch"/>
+            </template>
+        </el-input>
+    </div>
+</template>
+
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Search } from '@element-plus/icons-vue'
+const inputsearch = ref('')
+const onsearch = () => {
+    console.log("search: "+inputsearch)
+}
+
+</script>
